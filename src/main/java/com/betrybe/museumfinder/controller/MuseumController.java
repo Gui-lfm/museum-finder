e15844652a4c231df4501c8d671e80d3d0c69206
Guilherme Lucena <guilherme.lucena17@gmail.com>
2023-09-01 19:42:13 -0300

    req 06

--- src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -2,13 +2,17 @@ package com.betrybe.museumfinder.controller;
 
 
 import com.betrybe.museumfinder.dto.MuseumCreationDto;
+import com.betrybe.museumfinder.dto.MuseumDto;
+import com.betrybe.museumfinder.model.Coordinate;
 import com.betrybe.museumfinder.model.Museum;
 import com.betrybe.museumfinder.service.MuseumServiceInterface;
 import com.betrybe.museumfinder.util.ModelDtoConverter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +37,19 @@ public class MuseumController {
     Museum request = ModelDtoConverter.dtoToModel(newMuseum);
 
     return ResponseEntity.status(HttpStatus.CREATED).body(service.createMuseum(request));
+  }
+
+  /**
+   * Procura o museu mais próximo à localização informada.
+   */
+  @GetMapping("/closest")
+  public ResponseEntity<MuseumDto> getMuseumByLocation(
+      @RequestParam("lat") double latitude,
+      @RequestParam("lng") double longitude,
+      @RequestParam("max_dist_km") double maxDistance) {
+
+    Coordinate coordinate = new Coordinate(latitude, longitude);
+    Museum response = service.getClosestMuseum(coordinate, maxDistance);
+    return ResponseEntity.ok().body(ModelDtoConverter.modelToDto(response));
   }
 }
